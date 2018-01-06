a3e983cd4a9070b61e79c5bc7a07dc237495a1da
François Laplaud <elfrancesco+git@gmail.com>
2018-01-06 15:45:35 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,57 @@
+# RaiBlocks Wallet Installer for Linux
+
+
+As asked by a friend,
+Here is an easy to use script that will install the required dependancies to run the RaiBlocks wallet under linux
+
+Currently i have concentrated on making it work on Ubuntu since most of new linux user will use this distribution.
+
+
+## How does it work ?
+
+The script will :
+- Install docker.io if not already installed
+- Allow your user to run it without being root
+- Add extra autorizations so that docker container can run the wallet on your X server
+- Download my prebuild docker image containing the rai_wallet (if you want to build your own, you can get my Dockerfile from its dedicated [repository](https://github.com/rebrec/raiwallet_docker_container))
+- Create a shortcut that will run the wallet within the container.
+
+
+## How to install ?
+
+Simple as typing into a terminal : 
+
+``` bash
+wget -O - https://raw.githubusercontent.com/rebrec/raiwallet_installer/master/install.sh | sudo bash
+```
+You will be asked to type your user password so that the installer can do its stuff.
+
+
+
+## How to Use my freshly installed wallet ?
+
+Just search for "Raiblocks Wallet" on your application menu.
+
+Data to backup is stored into the RaiBlocks folder of your HOME directory.
+
+
+## Potential improvements
+
+Depending on the feedback from users, here is what i could do to improve this project :
+
+- Make it work on other Linux distributions
+- Make distribution packages (more fast and easy to install and UPDATE
+  - For Ubuntu
+  - For Other distributions.
+- Other suggestions ?
+
+
+## Feedback / Issues
+
+Feel free to open an issue [here](https://github.com/rebrec/raiwallet_installer/issues) if things are not working as expected for you, etc.
+
+
+## Donation
+
+My donation address is here : xrb_13dooo43obdz44fock4yh76gfs5w6uyfyodqmkm33tf6ou5uumafmjoohj1o
+
---
+++ install.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+
+SCRIPT_PATH="$( cd "$(dirname "$0")" ; pwd -P  )"
+DESTINATION_PATH=/opt/raiblocks/wallet/
+SHORTCUT_PATH=$HOME/.local/share/applications
+SHORTCUT_ICON_NAME=Raiblocks_Logo.png
+SHORTCUT_NAME=RaiBlocksWallet
+WALLET_FOLDER_NAME=RaiBlocks
+DOCKER_IMAGE_NAME=rebrec/rai_wallet
+DOCKER_RUN_FILE=/root/rai_wallet
+
+
+if [[ -z "${SUDO_USER}"  ]]; then
+    echo "To work, this script need to be run using sudo : "
+    echo "sudo $0"
+    exit -1
+fi
+USER="${SUDO_USER}"
+
+mkdir -p "${$DESTINATION_PATH}"
+
+# Build of the shortcut
+echo "[+] Building shortcut to ${DESTINATION_PATH}/${SHORTCUT_NAME}.desktop"
+cat > "${DESTINATION_PATH}/${SHORTCUT_NAME}.desktop" << EOF
+[Desktop Entry]
+Encoding=UTF-8
+Name=rai_wallet
+Comment=RaiBlocks Wallet Docker container
+Exec=/bin/bash "sg docker docker run -d -e DISPLAY=\$DISPLAY -e USER=\$(id -u) -e GROUP=\$(id -g) -v /tmp/.X11-unix/:/tmp/.X11-unix -v \$HOME/$WALLET_FOLDER_NAME:/root/$WALLET_FOLDER_NAME -ti --rm $DOCKER_IMAGE_NAME $DOCKER_RUN_FILE"
+Icon=$DESTINATION_PATH/$SHORTCUT_ICON_NAME
+Categories=Application;Cryptocurrencies;Raiblocks;Wallet
+Version=1.0
+Type=Application
+Terminal=0
+EOF
+
+# Build of the uninstall script
+echo "[+] Building the uninstall script to ${DESTINATION_PATH}/uninstall.sh"
+
+cat > "${DESTINATION_PATH}/uninstall.sh" << EOF
+#!/bin/sh
+
+SCRIPT_PATH=$SCRIPT_PATH
+DESTINATION_PATH=$DESTINATION_PATH
+SHORTCUT_PATH=$SHORTCUT_PATH
+SHORTCUT_ICON_NAME=$SHORTCUT_ICON_NAME
+SHORTCUT_NAME=$SHORTCUT_NAME
+WALLET_FOLDER_NAME=$WALLET_FOLDER_NAME
+DOCKER_IMAGE_NAME=$DOCKER_IMAGE_NAME
+DOCKER_RUN_FILE=$DOCKER_RUN_FILE
+
+rm -f \$SHORTCUT_PATH/\$SHORTCUT_NAME.desktop
+rm -rf \$DESTINATION_PATH
+sg docker docker rmi \$DOCKER_IMAGE_NAME
+
+update-desktop-database
+
+echo Uninstallation done
+EOF
+
+echo "[+] Installing docker.io"
+apt-get install --assume-yes docker.io
+
+echo "[+] Adding $USER to 'docker' group"
+adduser $USER docker
+
+echo "[+] Adding X Server autorizations"
+xhost local:root
+
+echo "[+] Download prebuild docker image"
+sg docker "docker pull ${DOCKER_IMAGE_NAME}"
+
+echo "[+] Copy Image to $DESTINATION_PATH"
+cp "${SCRIPT_PATH}/${SHORTCUT_ICON_NAME}" "${DESTINATION_PATH}"
+
+echo "[+] Copy the shortcut to $SHORTCUT_PATH"
+cp "${SCRIPT_PATH}/${SHORTCUT_NAME}.desktop" "${SHORTCUT_PATH}"
+
+echo "[+] Update Desktop Icons"
+update-desktop-database
+
+
+
+echo "********************************************"
+echo "********************************************"
+echo "****************        ********************"
+echo "****************  Done  ********************"
+echo "****************        ********************"
+echo "********************************************"
+echo "********************************************"
+echo "****                                    ****"
+echo "****   RaiBlocks Wallet is installed    ****"
+echo "****   and should be available on your  ****"
+echo "****   desktop and your Application     ****"
+echo "****   Menu                             ****"
+echo "****                                    ****"
+echo "********************************************"
+echo "********************************************"
+echo "********************************************"
